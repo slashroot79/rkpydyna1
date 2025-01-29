3ee011103e5b1aed16e8e40fb0663ba89b38ec87
Ragu Karuturi <ragukaruturi@microsoft.com>
2025-01-29 14:57:58 -0600

    changes3

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-#!/bin/bash
-# Install Dynatrace OneAgent
-# curl -o Dynatrace-OneAgent.sh https://<your-environment-id>.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?Api-Token=<your-api-token>
+# wget -O Dynatrace-OneAgent-Linux-x86-1.305.98.20250114-172617.sh "https://yfs56792.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86" --header="Authorization: Api-Token dt0c01.FB5JNMVMBJFQR67H6PFSYAHC.7R6GLQR6R4GWCMDPT5BEGTKE2NQGOFEJ6QGO2UEK6ZUDD5OBWCCQRF3RESZ3ZXCD"
+
+# wget https://ca.dynatrace.com/dt-root.cert.pem ; ( echo 'Content-Type: multipart/signed; protocol="application/x-pkcs7-signature"; micalg="sha-256"; boundary="--SIGNED-INSTALLER"'; echo ; echo ; echo '----SIGNED-INSTALLER' ; cat Dynatrace-OneAgent-Linux-x86-1.305.98.20250114-172617.sh ) | openssl cms -verify -CAfile dt-root.cert.pem > /dev/null
+
 # chmod +x Dynatrace-OneAgent.sh
-# ./Dynatrace-OneAgent.sh
 
-export PYTHONPATH=home/site/wwwroot/antenv/lib/python3.11/site-packages
+# /bin/sh Dynatrace-OneAgent-Linux-x86-1.305.98.20250114-172617.sh --set-monitoring-mode=fullstack --set-app-log-content-access=true
+
+# export PYTHONPATH=home/site/wwwroot/antenv/lib/python3.11/site-packages
 echo "******************* in start.sh *************************************************"
 # Start your Flask app with Gunicorn
-gunicorn --bind 0.0.0.0:8000 wsgi:app
+# gunicorn --bind 0.0.0.0:8000 wsgi:app
