884851aadd2716892404ac8b5bf1d0cbc684c50e
Ragu Karuturi <ragukaruturi@microsoft.com>
2025-01-29 14:37:47 -0600

    added post deployment test script

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Run the Gunicorn server
-gunicorn --bind=0.0.0.0:8000 app:app
+#!/bin/bash
+# Install Dynatrace OneAgent
+# curl -o Dynatrace-OneAgent.sh https://<your-environment-id>.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?Api-Token=<your-api-token>
+# chmod +x Dynatrace-OneAgent.sh
+# ./Dynatrace-OneAgent.sh
+
+echo "******************* in start.sh *************************************************"
+# Start your Flask app with Gunicorn
+gunicorn --bind 0.0.0.0:8000 wsgi:app
